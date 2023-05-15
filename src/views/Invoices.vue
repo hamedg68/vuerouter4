3217788005b19be5944fa432e6dfff2cf31a4836
hamed <hamedgorji1368@gmail.com>
2023-05-15 08:39:20 +0330

    feat: route guard in component

--- src/views/Invoices.vue
+++ src/views/Invoices.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>
-        <h1>Invoices</h1>
-    </div>
+  <div>
+    <h1>Invoices</h1>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { onBeforeRouteLeave } from "vue-router";
+
+onBeforeRouteLeave((to, from) => {
+  const answer = window.confirm("are you sudr you want to leave");
+  if (!answer) return false;
+});
+</script>
